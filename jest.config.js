7cd298fab0ca3a58ddf9f11b2e916d7041fc1287
josimarrocha <rocha_josimar@yahoo.com.br>
2021-07-16 23:28:23 -0300

    fix: import tsconfig and jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
