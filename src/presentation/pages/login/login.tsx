7c2d1bf8056a8c466b9db9835c62e3354a0720fb
josimarrocha <rocha_josimar@yahoo.com.br>
2021-09-08 23:18:42 -0300

    feat: add app router

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -8,13 +8,13 @@ const Login: FC = () => {
       <LoginHeader />
       <form action="" className={Styles.form}>
         <h2>Login</h2>
-        <Input type="email" name="email" placeholder="Digite seu e-mail"/>
-        <Input type="password" name="password" placeholder="Digite sua senha"/>
+        <Input type="email" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button className={Styles.submit} type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <FormStatus />
       </form>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
