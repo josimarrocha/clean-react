e5a331a71dcb1144eba38abda550886afde14fed
josimarrocha <rocha_josimar@yahoo.com.br>
2021-10-18 22:56:51 -0300

    feat: ensure HttpClient call with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,15 @@
+import { HttpPostClient } from '@/data/protocols/http'
+import { AccountModel } from '@/domains/models'
+import { AddAccountParams, AddAccount } from '@/domains/usecases'
+
+export class RemoteAddAccount implements AddAccount {
+  // recebe a url e o client(axios/fetch/outros)
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>) { }
+
+  async add(params: AddAccountParams): Promise<AccountModel> {
+    await this.httpPostClient.post({ url: this.url })
+    return await Promise.resolve({ accessToken: 'pass' })
+  }
+}
