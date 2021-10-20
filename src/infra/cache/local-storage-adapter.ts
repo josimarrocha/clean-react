c9eaf079afab7370ce0c18e64950f64d593bdf51
josimarrocha <rocha_josimar@yahoo.com.br>
2021-10-19 21:55:07 -0300

    fix: remove import jest-localstorage-mock

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,6 +1,5 @@
 
 import { SetStorage } from '@/data/protocols/cache/set-storage'
-import 'jest-localstorage-mock'
 
 export class LocalStorageAdapter implements SetStorage {
   async set(key: string, value: any): Promise<void> {
