880646099f01642344a2987b0b1b6b77513c7774
josimarrocha <rocha_josimar@yahoo.com.br>
2021-11-09 19:34:51 -0300

    test: ensure Login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('/login')
@@ -10,5 +12,14 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
     cy.getByTestId('email').should('not.have.length')
     cy.getByTestId('password').should('not.have.length')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').type(faker.random.word())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('password').type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
