068d7229cca6e520dc3f33c3640c503918eafe1f
josimarrocha <rocha_josimar@yahoo.com.br>
2021-11-09 19:23:53 -0300

    test: ensure Login load with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,14 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('/login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório!')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo obrigatório!')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+    cy.getByTestId('email').should('not.have.length')
+    cy.getByTestId('password').should('not.have.length')
   })
 })
