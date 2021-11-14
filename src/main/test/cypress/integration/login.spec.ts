bc9f64fec575bcf13458012f257d64baf57dd834
josimarrocha <rocha_josimar@yahoo.com.br>
2021-11-14 18:55:15 -0300

    test ensure Login will not submit form if is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -138,4 +138,20 @@ describe('Login', () => {
     cy.getByTestId('submit').dblclick()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    const accessToken = faker.datatype.uuid()
+    cy.intercept({
+      method: 'POST',
+      url: /login/
+    }, {
+      statusCode: 200,
+      body: {
+        accessToken
+      }
+    }).as('request')
+
+    cy.getByTestId('email').type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
